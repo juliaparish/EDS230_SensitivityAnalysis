34062afc3df8200fab4d0ad652111fb4ae18bcbf
julietcohen <jscohen@bren.ucsb.edu>
2022-04-21 10:59:36 -0700

    made graphs, fixed function

--- atmcon.R
+++ atmcon.R
@@ -14,16 +14,16 @@
 #' @examples
 atmcon <- function(vm, h, zm = (h + 200), k = 0.1, kd = 0.7){
   
-  # how can we include the standard deviation for windpseed and the error for h? 
-  
   zd = kd*h
   
   z = k*h
   
   ac = vm / (6.25*(ln((zm - zd)/z))**2)
   
-  ac_rounded <- round(ac, 2)
-  
-  return(paste0("The atmospheric conductance for this vegetation is ", ac_rounded, " {units}."))
+  return(list(ac = ac))
+  # names the output and set equal to itself so you can call the output with LHS
   
 }
+
+
+
